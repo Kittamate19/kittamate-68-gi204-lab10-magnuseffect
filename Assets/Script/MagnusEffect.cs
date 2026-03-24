388f58128579ca6f8d5099da67eabee6c901a0aa
Kittamate19 <dream19347@gmail.com>
2026-03-24 16:31:20 +0700

    Lab10

---
+++ Assets/Script/MagnusEffect.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MagnusEffect : MonoBehaviour
+{
+    public float kickForce = 1.0f;
+    public float spinAmount = 1.0f; // ��Ѻ��ȷҧ��ѧ������� ����/���
+    public float magnusStrength = 0.5f;
+    private Rigidbody _rb;
+    private bool _isShoot = false;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && !_isShoot)
+        {
+            // ��Ѻ��ȷҧ�͹�� x, y, z
+            _rb.AddForce(0, 4f, kickForce, ForceMode.Impulse);
+            // ����١�����ع��������͹���
+            _rb.AddRelativeTorque(Vector3.up * spinAmount);
+            _isShoot = true;
+        }
+    }
+    void FixedUpdate()
+    {
+        if (!_isShoot) return;
+        Vector3 velocity = _rb.linearVelocity;
+        Vector3 spin = _rb.angularVelocity;
+        // Cross Product �ҷ�ȷҧ�������������͹����
+        Vector3 magnusForce = Vector3.Cross(spin, velocity);
+        magnusForce *= magnusStrength;
+        _rb.AddForce(magnusForce);
+    }
+}
---
+++ Assets/Script/TorqueControls.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TorqueControls : MonoBehaviour
+{
+    public float torquePower = 1f;
+    private Rigidbody _rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Keyboard.current.dKey.isPressed)
+        {
+            _rb.AddTorque(0, torquePower, 0);
+        }
+    }
+}
